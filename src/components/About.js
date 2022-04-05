425e42c2afe8f1de459422b5d1b0a120f6f764fc
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-05 09:01:14 -0500

    submitting

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import user from "../data/user"
+import Links from "./Links"
+console.log(user.links)
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
